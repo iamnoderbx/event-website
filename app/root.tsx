ffd8cf12141a56856e3cf67a68717de9e9d494dd
iamnoderbx <mdfiles04@gmail.com>
2024-09-06 12:28:28 -0400

    project setup initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -4,20 +4,40 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+
 import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
+import { themeSessionResolver } from "./session/sessions.server";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import clsx from "clsx";
+
+
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>()
+  const [ theme ] = useTheme()
+
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -25,6 +45,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  )
 }
